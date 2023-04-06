ef412f817c3198a572cf32e3c8e1c9c3215d71a7
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-04-06 16:25:48 -0700

    -

--- src/routes/food.js
+++ src/routes/food.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const Food = require('../models/food');
+const {Food} = require('../models/food');
 
 router.get('/', readFood);
 router.get('/:id', readOneFood);
